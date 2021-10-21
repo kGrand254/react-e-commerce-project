85f8c342924757088a5728b60615b6ff3da15e4f
kGrand254 <kevinkimutai62@gmail.com>
2021-10-21 14:15:56 +0300

    ccreated react app using create-react-app tool and added banner component

---
+++ src/banner.component.jsx
@@ -0,0 +1,72 @@
+import "./banner.component.styles.scss";
+
+function Banner() {
+  return (
+    <div className="container homepage">
+      <div className="row">
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">Headphones</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">consoles</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">Tv's</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">Phones</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">Cameras</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6">
+          <div className="menu-card">
+            <div className="menu-item">
+              <div className="content">
+                <h2 className="title">VR headset</h2>
+                <h4 className="subtitle">shop now</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
